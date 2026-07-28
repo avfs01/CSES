1f985c06f44bf56b6acbfdd7dc2e704bebeac144
avfs01 <antoniovitorunb@gmail.com>
2026-07-28 11:04:50 -0300

    feat: solution to the "Missing Number" problem

--- missingNumber.cpp
+++ missingNumber.cpp
@@ -2,11 +2,18 @@
 
 using namespace std;
 
+// Soma de Gauss
+long long somaN(long long n){
+	return ((1+n)*n)/2;
+}
+
 int main(){
-  long long int n,x;
-  cin >> n;
-  for (int i = 0; i < n-1; i++){
-    cin >> x;
-  }
-  return 0;
+	long long n,soma=0,x; cin >> n;
+	for (int i = 0; i < n-1; i++)
+	{
+		cin >> x;
+		soma += x;
+	}
+	cout << somaN(n) - soma << '\n';
+	return 0;
 }
